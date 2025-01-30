5f8428595b109e513ff10ab81744f661cec905ec
csd3761 <csd3761@csd.uoc.gr>
2025-01-30 20:08:43 +0200

    Implement Exception Middleware

---
+++ CountriesApi.Presentation/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountriesApi.Presentation.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _environment;
+
+        public ExceptionMiddleware(
+            RequestDelegate next,
+            ILogger<ExceptionMiddleware> logger,
+            IHostEnvironment environment
+        )
+        {
+            _next = next;
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unhandled exception occured.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var statusCode = (int)HttpStatusCode.InternalServerError;
+            var problem = new ProblemDetails();
+
+            switch (ex)
+            {
+                default:
+                    problem = new ProblemDetails
+                    {
+                        Title = "Internal Server Error",
+                        Detail = _environment.IsDevelopment() ? ex.Message : "An error occured",
+                        Status = statusCode,
+                        Instance = context.Request.Path
+                    };
+                    break;
+            }
+
+            context.Response.StatusCode = statusCode;
+            await context.Response.WriteAsJsonAsync( problem );
+        }
+    }
+}
