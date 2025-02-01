916fa00df5e3a9196245e9ee2d6b69d204c5d4b8
csd3761 <csd3761@csd.uoc.gr>
2025-02-02 00:52:52 +0200

    Set up redis appSettings and install Scrutor for decorator pattern

---
+++ CountriesApi.Domain/Configuration/RedisSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountriesApi.Domain.Configuration
+{
+    public class RedisSettings
+    {
+        public const string SectionName = "RedisSettings";
+        public string ConnectionString { get; set; }
+        public int CacheDurationMinutes { get; set; }
+    }
+}
